a2a6a50b51e2c234f85421bc7d3421d00e3c9a12
EmilioAK <57467368+EmilioAK@users.noreply.github.com>
2021-01-04 01:54:38 +0100

    Even better version

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,13 +1,5 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below", "below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings_of_word (word, dictionary)
-    dictionary.reject {|dict_word| word.downcase.scan(dict_word) == []}
-end
-
-def substrings (string, dictionary)
-    words = string.scan(/[\w']+/)
-    words.each_with_object({}) do |word, hash|
-        substrings = substrings_of_word(word, dictionary)
-        substrings.each {|substring| hash[substring] ? hash[substring] += 1 : hash[substring] = 1}
-    end
+def substrings(string, dictionary)
+  dictionary.map{|word| [word, string.scan(word).count]}.to_h
 end
