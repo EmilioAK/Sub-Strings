ca43526486d08d49327e0cbcefee91ccf11f8fa3
EmilioAK <57467368+EmilioAK@users.noreply.github.com>
2021-01-04 02:09:16 +0100

    Even fucking better version

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,5 +1,5 @@
 dictionary = ["below", "below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, dictionary)
-  dictionary.map{|word| [word, string.scan(word).count]}.to_h
+    dictionary.to_h{|word| [word, string.scan(word).count]}
 end
